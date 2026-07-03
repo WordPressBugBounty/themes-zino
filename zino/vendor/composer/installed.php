4e1cfd1f365a21cf20d7c49696f156213e5cfe12
Sean Murphy <sean@iamseanmurphy.com>
2026-07-03 08:24:48 +0000

    1.2.5

--- zino/vendor/composer/installed.php
+++ zino/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a80efd8768c99bfedba28404c2e066b19413990d',
+        'reference' => 'f9c5f9b3950eff1683ab1d862b705a0ba5a3e80a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a80efd8768c99bfedba28404c2e066b19413990d',
+            'reference' => 'f9c5f9b3950eff1683ab1d862b705a0ba5a3e80a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
